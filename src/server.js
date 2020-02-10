7a9fb2a96b9b1ebcce3160f1a21188c581164bc3
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-02-10 05:04:33 -0500

    heroku adjustments

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import AlmostSimplyOne from "./AlmostSimplyOne";
+import AlmostSimplyOne from "./boardgame.io/AlmostSimplyOne";
 require('dotenv').config()
 import path from 'path';
 import serve from 'koa-static';
@@ -10,16 +10,15 @@ const PORT = process.env.PORT || 8000;
 
 const server = Server({
   games: [AlmostSimplyOne],
-
-  db: new Mongo({
-      // url: 'mongodb://localhost:27017',
-      url: `mongodb+srv://jimmy:${process.env.MONGODB_PASSWORD}@cluster0-czkry.mongodb.net/test?retryWrites=true&w=majority`,
-      dbname: 'almost-simply-one1'
-  }),
+  // db: new Mongo({
+  //     // url: 'mongodb://localhost:27017',
+  //     url: `mongodb+srv://jimmy:${process.env.MONGODB_PASSWORD}@cluster0-czkry.mongodb.net/test?retryWrites=true&w=majority`,
+  //     dbname: 'almost-simply-one1'
+  // }),
 });
 
 const { app } = server;
-const root = path.join(__dirname, './build');
+const root = path.join(__dirname, '../build');
 app.use(
   historyApiFallback({ index: 'index.html', whiteList: ['/api', '/games'] })
 );
