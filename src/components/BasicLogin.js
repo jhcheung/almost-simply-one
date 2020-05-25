18a632c983a4d255d36ac9a7f61c7edfff8ad1a3
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-05-24 21:51:14 -0400

    cleaned up some comments

--- src/components/BasicLogin.js
+++ src/components/BasicLogin.js
@@ -5,7 +5,6 @@ function BasicLogin(props) {
     const [name, setName] = useState("");
     const handleSubmit = (e) =>{
         e.preventDefault();
-        // resetField("roomName")
         props.loginDispatch({ name: name })
         props.onEnterLobby(name)
         setName("")
@@ -18,48 +17,18 @@ function BasicLogin(props) {
     return(
         <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
-          {/* <Header as='h2' color='teal' textAlign='center'>
-            <Image src='/logo.png' /> 
-            Almost Simply One
-          </Header> */}
           <Form size='small' onSubmit={handleSubmit}>
             <Segment stacked>
               <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' value={name} onChange={handleChange} />
-              {/* <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-              /> */}
   
               <Button color='teal' fluid size='large'>
                 Login
               </Button>
             </Segment>
           </Form>
-          {/* <Message>
-            New to us? <Link to='/signup'>Sign Up</Link>
-          </Message> */}
         </Grid.Column>
       </Grid>    
-  
-
-
-        // <Form onSubmit={handleSubmit}>
-        //     <Form.Group widths='equal'>
-        //     <Form.Input 
-        //             fluid label='name' 
-        //             placeholder='name' 
-        //             name="name" 
-        //             value={name} 
-        //             onChange={handleChange} 
-        //     />
-        //     </Form.Group>
-
-        //     <Button type='submit'>Submit</Button>
-        // </Form>
-    )
+      )
 }
 
 export default BasicLogin
